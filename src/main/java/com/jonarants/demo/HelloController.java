0c7616cb3910eef2fa642fcf7138ebaf2c99dd0b
Jonathan Vazquez Duran <jonathan.vazquezd@outlook.es>
2026-06-13 20:21:32 -0600

    Testing functions and comments

--- src/main/java/com/jonarants/demo/HelloController.java
+++ src/main/java/com/jonarants/demo/HelloController.java
@@ -8,10 +8,19 @@ public class HelloController {
     
     @GetMapping("/")
     public String index() {
+        System.out.println("Hitting root path");
         return "Greetings from Spring Boot!";
     }
+
+    @GetMapping("/test") 
+    public String test() {
+        System.out.println("Hitting test path");
+        return "Test from path test";
+    }
+
     @GetMapping({"/v1/testing"})
     public String multiPathEndpoint() {
+        System.out.println("Hitting v1 testing");
         return "Testing from SpringBoot";
     }
 }
